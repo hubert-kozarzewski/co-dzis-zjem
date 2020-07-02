6eff455592d51c12d9e4668bc85772a0cabaa2e9
Hubert Kozarzewski <kozarzewski.hubert@gmail.com>
2020-07-02 20:31:18 +0200

    [feature] Add Ingredient table

---
+++ resources/views/ingredients/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                    <h1>ogarniesz? ogarnę.</h1>
+                <div class="card-body">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
